c54e499fc1280bc2b27e4b446c796abed0a900f2
Leonid Pliushch <leonid.pliushch@gmail.com>
2020-11-19 15:20:30 +0200

    install.sh: db is at $PREFIX/share

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 NAME="whatprovides"
-mkdir -p $PREFIX/opt/${NAME}
-cp -r db $PREFIX/opt/${NAME}
+mkdir -p $PREFIX/share/${NAME}
+cp -r db $PREFIX/share/${NAME}
 cp ${NAME} $PREFIX/bin
 
 echo "Installation finished. execute $NAME -h for help"
